efa37de69f7c5fd51be61082ee834a43ec7fd0db
bell25bowman-jade <bell25bowman@gmail.com>
2026-06-27 16:00:13 -0400

    begin deploy

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+
+class ProductionConfig:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI)
+    CACHE_TYPE = "SimpleCache"
 
 class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:812288@localhost/mechanicshop'
