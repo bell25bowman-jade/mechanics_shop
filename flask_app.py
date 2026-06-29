ab1bb3f4f1199e02399f2003d79119c85121da80
bell25bowman-jade <bell25bowman@gmail.com>
2026-06-29 00:17:34 -0400

    internal db

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,5 @@
 from app.__init__ import create_app
-from app.models import db
 
 
 
 app = create_app("ProductionConfig")
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run()
